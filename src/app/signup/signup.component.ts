265ae0a159fc6255925d1dd576882b8fd1361dfd
amirlahyani <mohamedamirlahyani@gmail.com>
2025-05-08 13:45:43 +0100

    Ajout du projet Angular complet

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,105 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { SnackbarService } from '../services/snackbar.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { GlobalConstants } from '../shared/global-constants';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+})
+export class SignupComponent implements OnInit {
+  //signupForm: any = FormGroup;
+  signupForm!: FormGroup;
+
+  responseMessage: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private snackbarService: SnackbarService,
+    private dialogRef: MatDialogRef<SignupComponent>,
+    private ngxService: NgxUiLoaderService
+  ) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      name: [
+        null,
+        [Validators.required, Validators.pattern(GlobalConstants.nameRegex)],
+      ],
+      email: [
+        null,
+        [Validators.required, Validators.pattern(GlobalConstants.emailRegex)],
+      ],
+      contactNumber: [
+       null,
+        [
+          Validators.required,
+          Validators.pattern(GlobalConstants.contactNumberRegex),
+         
+        ],
+      ],
+      password: [null, [Validators.required]],
+    });
+  }
+
+
+
+  onPhoneNumberInput(event: any) {
+    // Récupère seulement les chiffres saisis
+    let inputValue = event.target.value.replace(/\D/g, '');
+    // Limite à 8 chiffres
+    inputValue = inputValue.substring(0, 8);
+    
+    // Met à jour la valeur complète (+216 + les 8 chiffres)
+    this.signupForm.patchValue({
+      contactNumber: '+216' + inputValue
+    }, {emitEvent: false});
+    
+    // Met à jour l'affichage du champ (seulement les 8 chiffres)
+    event.target.value = inputValue;
+  }
+
+
+
+
+
+  handleSubmit() {
+    this.ngxService.start();
+    var formData = this.signupForm.value;
+    var data = {
+      name: formData.name,
+      email: formData.email,
+      contactNumber: formData.contactNumber,
+      password: formData.password,
+    };
+    this.userService.signup(data).subscribe(
+      (response: any) => {
+        this.ngxService.stop();
+        this.dialogRef.close();
+        this.responseMessage = response?.message;
+        this.snackbarService.openSnackBar(this.responseMessage, '');
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        this.ngxService.stop();
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackbarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
+ 
+}
